f9d3ae98a6c81fd713f7ed910faf88c7f274363c
lionel <lionel_pr@hotmail.com>
2023-03-16 10:58:42 +0100

    [Persistence] Config constant data (Clubs and categories)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
-INSERT INTO TBL_EMPLOYEES (first_name, last_name, email) VALUES
-  ('Lokesh', 'Gupta', 'abc@gmail.com'),
-  ('Deja', 'Vu', 'xyz@email.com'),
-  ('Caption', 'America', 'cap@marvel.com');
+INSERT INTO TBL_CLUB (name) VALUES
+('Uckange'),
+('Florange'),
+('Rurange Montrequienne'),
+('Illange'),
+('Metzervisse'),
+('Ennery'),
+('Guénange'),
+('Thionville et Elange'),;
+
+INSERT INTO TBL_CATEGORY (name, year_min, year_max) VALUES
+('Poussinet (es)', 2017, 2018),
+('Mini-poussins (es)', 2015, 2016),
+('Poussins (es)', 2013, 2014),
+('Benjamins (es)', 2011, 2012);
