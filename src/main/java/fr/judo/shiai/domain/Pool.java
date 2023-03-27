1e2765018fc2e99e77f3ee47a4946c4311015030
lionel <lionel_pr@hotmail.com>
2023-03-27 14:35:34 +0200

    [Solver] Fix min and max pool size constraints

--- src/main/java/fr/judo/shiai/domain/Pool.java
+++ src/main/java/fr/judo/shiai/domain/Pool.java
@@ -3,7 +3,6 @@ package fr.judo.shiai.domain;
 import lombok.Getter;
 import lombok.Setter;
 import org.optaplanner.core.api.domain.entity.PlanningEntity;
-import org.optaplanner.core.api.domain.lookup.PlanningId;
 import org.optaplanner.core.api.domain.variable.InverseRelationShadowVariable;
 
 import java.util.ArrayList;
