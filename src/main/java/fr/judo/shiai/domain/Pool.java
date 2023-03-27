0e4585632343bf4851fe5bbe86f8758a34b3a3e9
lionel <lionel_pr@hotmail.com>
2023-03-27 11:20:11 +0200

    [Solver] Add min and max pool size constraints

--- src/main/java/fr/judo/shiai/domain/Pool.java
+++ src/main/java/fr/judo/shiai/domain/Pool.java
@@ -1,13 +1,43 @@
 package fr.judo.shiai.domain;
 
 import lombok.Data;
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.variable.InverseRelationShadowVariable;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
-@Data
+@PlanningEntity
 public class Pool {
 
     private Long id;
 
     private Category category;
+    private List<Judoka> judokas =  new ArrayList<>();
+
+
+    @InverseRelationShadowVariable(sourceVariableName = "pools")
+    public List<Judoka> getJudokas() {
+        return judokas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setJudokas(List<Judoka> judokas) {
+        this.judokas = judokas;
+    }
 }
--- src/main/java/fr/judo/shiai/solver/SenshiConstraintProvider.java
+++ src/main/java/fr/judo/shiai/solver/SenshiConstraintProvider.java
@@ -1,20 +1,27 @@
 package fr.judo.shiai.solver;
 
 import fr.judo.shiai.domain.Judoka;
+import fr.judo.shiai.domain.Pool;
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.Joiners;
 
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.count;
+
 public class SenshiConstraintProvider implements ConstraintProvider {
     private static final double PERCENTAGE_WEIGHT_DIFFERENCE = 0.1;
     private static final String WEIGHT_CONSTRAINT_LABEL = "weight range";
     private static final String CLUB_VARIETY_CONSTRAINT_LABEL = "club variety";
 
-    private static final  String GENDER_CONFLICT_CONSTRAINT_LABEL = "gender conflict";
+    private static final String GENDER_CONFLICT_CONSTRAINT_LABEL = "gender conflict";
 
-    private static final  String CATEGORY_CONFLICT_CONSTRAINT_LABEL = "category conflict";
+    private static final String CATEGORY_CONFLICT_CONSTRAINT_LABEL = "category conflict";
+
+    private static final String MIN_4_CONFLICT_CONSTRAINT_LABEL = "min pool size conflict";
+
+    private static final String MAX_4_CONFLICT_CONSTRAINT_LABEL = "max pool size conflict";
 
     @Override
     public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
@@ -22,11 +29,41 @@ public class SenshiConstraintProvider implements ConstraintProvider {
                 // Hard constraints
                 genderConflict(constraintFactory),
                 categoryConflict(constraintFactory),
+                minPoolSizeConflict(constraintFactory),
+                maxPoolSizeConflict(constraintFactory),
                 // Soft constraints
                 clubVariety(constraintFactory),
                 weightRange(constraintFactory)
         };
     }
+
+
+    /**
+     * @param constraintFactory manage all constraints
+     * @return Min 2 judokas per pool
+     */
+    Constraint minPoolSizeConflict(ConstraintFactory constraintFactory) {
+
+        return constraintFactory
+                .forEach(Pool.class)
+                .filter(pool -> pool.getJudokas().size() > 2)
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint(MIN_4_CONFLICT_CONSTRAINT_LABEL);
+    }
+
+    /**
+     * @param constraintFactory manage all constraints
+     * @return Max 4 judokas per pool
+     */
+    Constraint maxPoolSizeConflict(ConstraintFactory constraintFactory) {
+
+        return constraintFactory
+                .forEach(Pool.class)
+                .filter(pool -> pool.getJudokas().size() < 5)
+                .penalize(HardSoftScore.ONE_HARD)
+                .asConstraint(MAX_4_CONFLICT_CONSTRAINT_LABEL);
+    }
+
 
     /**
      * @param constraintFactory manage all constraints
