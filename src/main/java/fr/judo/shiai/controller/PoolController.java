c0383c85b9ad9271d3511340817827e4200af2cf
Jery <jludwig84@gmail.com>
2023-04-08 20:20:42 +0200

    - create a fallback method for generating the pools (using the naive approach)

--- src/main/java/fr/judo/shiai/controller/PoolController.java
+++ src/main/java/fr/judo/shiai/controller/PoolController.java
@@ -1,10 +1,9 @@
 package fr.judo.shiai.controller;
 
-import fr.judo.shiai.domain.Judoka;
-import fr.judo.shiai.domain.Pool;
-import fr.judo.shiai.domain.PoolDispatchingSolution;
+import fr.judo.shiai.domain.*;
 import fr.judo.shiai.dto.PoolDto;
 import fr.judo.shiai.mappers.PoolMapper;
+import fr.judo.shiai.repository.CategoryRepository;
 import fr.judo.shiai.repository.JudokaRepository;
 import fr.judo.shiai.solver.SenshiSolver;
 import lombok.extern.slf4j.Slf4j;
@@ -14,7 +13,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Slf4j
 @CrossOrigin
@@ -26,6 +30,8 @@ public class PoolController {
     @Autowired
     private JudokaRepository judokaRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Autowired
     private SenshiSolver senshiSolver;
@@ -50,6 +56,53 @@ public class PoolController {
             log.error(e.getMessage());
         }
         return poolMapper.toDto(poolDispatchingSolution.getPoolList());
+    }
+
+    @PostMapping("/pools-fallback")
+    public Iterable<PoolDto> computeAllPoolsFallback() {
+
+        List<Pool> pools = new ArrayList<>();
+        final AtomicLong poolIndexCounter = new AtomicLong();
+
+        // Retrieve judokas
+        List<Judoka> judokas = judokaRepository.findAllPresentAndWeightedJudoka();
+        Iterable<Category> categories = categoryRepository.findAll();
+
+        // create sub-groups of category and sex among the judokas present and weighted
+        categories.forEach(category -> {
+
+            Collection<Judoka> judokaInCategory = judokas.stream()
+                    .filter(judoka -> judoka.getCategory().getName().equals(category.getName()))
+                    .toList();
+
+            Collection<Judoka> boys = judokaInCategory.stream()
+                    .filter(judoka -> judoka.getGender().equals(Gender.MALE))
+                    .sorted(Comparator.comparing(Judoka::getWeight))
+                    .toList();
+            Collection<Judoka> girls = judokaInCategory.stream()
+                    .filter(judoka -> judoka.getGender().equals(Gender.FEMALE))
+                    .sorted(Comparator.comparing(Judoka::getWeight))
+                    .toList();
+
+            pools.addAll(buildPools(boys, poolIndexCounter));
+            pools.addAll(buildPools(girls, poolIndexCounter));
+        });
+
+        return poolMapper.toDto(pools);
+    }
+
+    private Collection<? extends Pool> buildPools(Collection<Judoka> judokas, AtomicLong poolIndexCounter) {
+        final AtomicInteger counter = new AtomicInteger();
+        return new ArrayList<>(
+                judokas.stream()
+                        .collect(Collectors.groupingBy(it -> counter.getAndIncrement() / MAX_JUDOKAS_PER_POOL))
+                        .values())
+                .stream().map(judokasSubSet -> {
+                    Pool pool = new Pool();
+                    pool.setId(poolIndexCounter.getAndIncrement());
+                    pool.setJudokaList(judokasSubSet);
+                    return pool;
+                }).toList();
     }
 
     /**
