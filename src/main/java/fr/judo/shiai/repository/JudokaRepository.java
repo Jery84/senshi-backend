4107710f4f67aaf954b8917671f3a3309100fadc
lionel <lionel_pr@hotmail.com>
2023-04-12 20:53:51 +0200

    [Solver] Load data from H2 in test using LoaderService and new query

--- src/main/java/fr/judo/shiai/repository/JudokaRepository.java
+++ src/main/java/fr/judo/shiai/repository/JudokaRepository.java
@@ -12,4 +12,9 @@ public interface JudokaRepository extends CrudRepository<Judoka, Integer> {
 
     @Query("SELECT j FROM Judoka j WHERE j.present = true and j.weight is not null and j.weight > 0")
     List<Judoka> findAllPresentAndWeightedJudoka();
+
+
+
+    @Query("SELECT j FROM Judoka j WHERE j.present =  true AND j.category.name = 'BENJAMIN' AND j.gender = 'MALE'")
+    List<Judoka> findBenjamins();
 }
--- src/test/java/SolverTest.java
+++ src/test/java/SolverTest.java
@@ -2,52 +2,26 @@ import fr.judo.shiai.ShiaiApp;
 import fr.judo.shiai.domain.Judoka;
 import fr.judo.shiai.domain.Pool;
 import fr.judo.shiai.domain.PoolDispatchingSolution;
+import fr.judo.shiai.repository.JudokaRepository;
+import fr.judo.shiai.service.LoaderService;
 import fr.judo.shiai.solver.SenshiSolver;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.kie.internal.io.ResourceFactory;
-import org.simpleflatmapper.csv.CsvMapperFactory;
-import org.simpleflatmapper.csv.CsvParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @SpringBootTest(classes = ShiaiApp.class)
 public class SolverTest {
+    @Autowired
+    LoaderService loaderService;
 
-
-    public static PoolDispatchingSolution generateDemoData() {
-        PoolDispatchingSolution poolDispatchingSolution = new PoolDispatchingSolution();
-        List<Judoka> judokas = new ArrayList<>();
-        try {
-            CsvParser.separator(';')
-                    .mapWith(
-                            CsvMapperFactory
-                                    .newInstance()
-                                    .defaultDateFormat("dd/MM/yyyy")
-                                    .newMapper(Judoka.class))
-                    .stream(ResourceFactory.newClassPathResource("Poussins.csv").getReader())
-                    .collect(Collectors.toCollection(() -> judokas));
-            log.info("" + judokas.size());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        List<Pool> poolList = new ArrayList<>();
-        for (int i = 0; i < judokas.size() / 4 + 1; i++) {
-            Pool pool = new Pool();
-            pool.setId(Long.valueOf(i));
-            poolList.add(pool);
-        }
-        poolDispatchingSolution.setPoolList(poolList);
-        poolDispatchingSolution.setJudokaList(judokas);
-        return poolDispatchingSolution;
-    }
+    @Autowired
+    JudokaRepository judokaRepository;
 
     public static boolean printSolution(PoolDispatchingSolution poolDispatchingSolution) {
         int judokasCount = 0;
@@ -76,9 +50,39 @@ public class SolverTest {
                 - pool.getJudokaList().stream().min(Comparator.comparing(Judoka::getWeight)).orElseThrow(IllegalStateException::new).getWeight());
     }
 
+    public PoolDispatchingSolution generateDemoData() {
+        PoolDispatchingSolution poolDispatchingSolution = new PoolDispatchingSolution();
+        List<Judoka> judokas = judokaRepository.findBenjamins();
+        /*try {
+            CsvParser.separator(';')
+                    .mapWith(
+                            CsvMapperFactory
+                                    .newInstance()
+                                    .defaultDateFormat("dd/MM/yyyy")
+                                    .newMapper(Judoka.class))
+                    .stream(ResourceFactory.newClassPathResource("Poussins.csv").getReader())
+                    .collect(Collectors.toCollection(() -> judokas));
+            log.info("" + judokas.size());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+
+
+        List<Pool> poolList = new ArrayList<>();
+        for (int i = 0; i < judokas.size() / 4 + 1; i++) {
+            Pool pool = new Pool();
+            pool.setId(Long.valueOf(i));
+            poolList.add(pool);
+        }
+        poolDispatchingSolution.setPoolList(poolList);
+        poolDispatchingSolution.setJudokaList(judokas);
+        return poolDispatchingSolution;
+    }
+
     @Test
     void firstTest() {
-        SenshiSolver senshiSolver= new SenshiSolver();
+        loaderService.load();
+        SenshiSolver senshiSolver = new SenshiSolver();
         printSolution(senshiSolver.solve(generateDemoData()));
     }
 }
