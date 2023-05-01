9ae7a2dcfc0af146253715c25eb7bdfd798f1ada
lionel <lionel_pr@hotmail.com>
2023-05-01 10:16:47 +0200

    [Solver] Add new "second choice" calculation to poolController

--- src/main/java/fr/judo/shiai/controller/PoolController.java
+++ src/main/java/fr/judo/shiai/controller/PoolController.java
@@ -5,6 +5,7 @@ import fr.judo.shiai.domain.Pool;
 import fr.judo.shiai.domain.PoolDispatchingSolution;
 import fr.judo.shiai.repository.JudokaRepository;
 import fr.judo.shiai.solver.SenshiSolver;
+import fr.judo.shiai.solver.SenshiSolverSecondChoice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
@@ -28,6 +30,8 @@ public class PoolController {
     @Autowired
     private SenshiSolver senshiSolver;
 
+    @Autowired
+    private SenshiSolverSecondChoice senshiSolverSecondChoice;
 
     @PostMapping("/pools")
     public List<Pool> computeAllPools() {
@@ -36,12 +40,25 @@ public class PoolController {
             // Retrieve judokas
             List<Judoka> judokas = judokaRepository.findAllPresentAndWeightedJudoka();
             log.info("Judokas available : " + judokas.size());
+
             // Declare pools
             List<Pool> pools = computePoolsList(judokas.size());
             // Declare problem/solution container
             poolDispatchingSolution.setPoolList(pools);
             poolDispatchingSolution.setJudokaList(judokas);
             poolDispatchingSolution = senshiSolver.solve(poolDispatchingSolution);
+
+            if (!allPoolsAreValid(poolDispatchingSolution.getPoolList())) {
+                pools.clear();
+                log.info("Recompute pools to try to get a better result");
+                poolDispatchingSolution = new PoolDispatchingSolution();
+                // Declare pools
+                pools = computePoolsList(judokas.size());
+                // Declare problem/solution container
+                poolDispatchingSolution.setPoolList(pools);
+                poolDispatchingSolution.setJudokaList(judokas);
+                poolDispatchingSolution = senshiSolverSecondChoice.solve(poolDispatchingSolution);
+            }
         } catch (Exception e) {
             log.error(e.getMessage());
         }
@@ -49,16 +66,31 @@ public class PoolController {
     }
 
     /**
-     * @param judokasCount
-     * @return
+     * @param judokasCount count all judokas in list
+     * @return list of pools according max size
      */
     private List<Pool> computePoolsList(final int judokasCount) {
         List<Pool> poolList = new ArrayList<>();
         for (int i = 0; i < judokasCount / MAX_JUDOKAS_PER_POOL + 1; i++) {
             Pool pool = new Pool();
-            pool.setId(Long.valueOf(i));
+            pool.setId((long) i);
             poolList.add(pool);
         }
         return poolList;
+    }
+
+    private boolean isPoolValid(final Pool pool) {
+        double maxWeight = pool.getJudokaList().stream().max(Comparator.comparing(Judoka::getWeight)).orElseThrow(IllegalStateException::new).getWeight();
+        double minWeight = pool.getJudokaList().stream().min(Comparator.comparing(Judoka::getWeight)).orElseThrow(IllegalStateException::new).getWeight();
+        return (pool.getJudokaList().size() >= 2 && pool.getJudokaList().size() < 5)
+                && (maxWeight * 0.1 > maxWeight - minWeight);
+    }
+
+    private boolean allPoolsAreValid(final List<Pool> pools) {
+        boolean res = true;
+        for (Pool pool : pools) {
+            res = res && isPoolValid(pool);
+        }
+        return res;
     }
 }
